ae83d2eb2598282d4f8a8dc5d1c35495001d7287
mary_lev <marylevchenko@gmail.com>
2020-10-10 23:20:39 +0300

    add comments

--- core/models.py
+++ core/models.py
@@ -32,24 +32,26 @@ class Lesson(models.Model):
 
 
 class TaskGroup(models.Model):
-	lesson = models.ForeignKey(Lesson)
-	text = models.TextField()
-	mark_max = models.IntegerField()
+	lesson = models.ForeignKey(Lesson) # связь с уроком
+	text = models.TextField() # общий текст для группы задач
+	#mark_max = models.IntegerField() # максимальное количество баллов за группу задач?
 
 
 class Task(models.Model):
-	task_group = models.ForeignKey(TaskGroup)
-	text = models.TextField()
-	question = models.TextField()
-	picture = models.ImageField()
-	solution = models.TextField()
+	task_group = models.ForeignKey(TaskGroup) # связь с группой задач
+	text = models.TextField() # формулировка задачи
+	question_type = models.TextField() # тип задачи (сделать список: выбор, вписать ответ)
+	options = models.TextField() # поле для хранения опций, если вопрос - выбор
+	picture = models.ImageField() # картинка из шаблона
+	solution = models.TextField(null=True) # образцовое решение
+	mark = models.IntegerField() # количество баллов за задачу (здесь или в Solution?)
 
 
-class Solution(models.Model):
-	task = models.ForeignKey(Task)
-	text = models.CharField()
-	student = models.ForeignKey(Student)
-	mark = models.IntegerField()
+class Solution(models.Model): # решение конкретной задачи конкретным студентом
+	task = models.ForeignKey(Task) # связь с задачей
+	text = models.CharField() # решение
+	student = models.ForeignKey(Student) # связь со студентом
+	mark = models.IntegerField(default=0) # количество полученных баллов
 
 
 class Feedback(models.Model):
