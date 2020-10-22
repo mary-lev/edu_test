8e0ec50a5d24680973e3d8fbf74902ba8f9780cb
mary_lev <marylevchenko@gmail.com>
2020-10-23 02:18:59 +0300

    add django auth

--- edu_test/urls.py
+++ edu_test/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('', include('core.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
