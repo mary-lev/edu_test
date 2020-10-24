f2be48045191a882fc565ac5df588f894a608b09
mary_lev <marylevchenko@gmail.com>
2020-10-24 22:05:48 +0300

    parse sheets

---
+++ sheets/utils.py
@@ -0,0 +1,36 @@
+from pprint import pprint
+
+import httplib2
+import apiclient.discovery
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+# Файл, полученный в Google Developer Console
+CREDENTIALS_FILE = 'sheets/creds1.json'
+# ID Google Sheets документа (можно взять из его URL)
+spreadsheet_id = '1XLlJLyQP69i155tzMlMLoHZM_B83XgvTxQ6eazuFuA0'
+
+# Авторизуемся и получаем service — экземпляр доступа к API
+credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    CREDENTIALS_FILE,
+    ['https://www.googleapis.com/auth/spreadsheets',
+     'https://www.googleapis.com/auth/drive'])
+
+httpAuth = credentials.authorize(httplib2.Http())
+service = apiclient.discovery.build('sheets', 'v4', http = httpAuth)
+
+# Пример чтения файла
+spreadsheet = service.spreadsheets().get(
+    spreadsheetId=spreadsheet_id,
+    ).execute()
+
+titles = []
+
+for sheet in spreadsheet['sheets']:
+    titles.append(sheet['properties']['title'])
+
+values = service.spreadsheets().values().get(
+    spreadsheetId=spreadsheet_id,
+    range=titles[0]+"!A1:Y75",
+    majorDimension='ROWS'
+    ).execute()
--- sheets/views.py
+++ sheets/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from .utils import values
 
 def index(request):
-	return render(request, 'sheets/index.html', {})
+	return render(request, 'sheets/index.html', {'values': values})
