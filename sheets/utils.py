1106262f5a069dcc8dcde7aed87a4c403d47a6a5
mary_lev <marylevchenko@gmail.com>
2020-10-25 00:35:32 +0300

    extract from mio4 to json

--- sheets/utils.py
+++ sheets/utils.py
@@ -1,3 +1,4 @@
+import json
 import pandas as pd
 import httplib2
 import apiclient.discovery
@@ -31,9 +32,41 @@ for sheet in spreadsheet['sheets']:
 values = service.spreadsheets().values().get(
     spreadsheetId=spreadsheet_id,
     range=titles[0]+"!A1:Y75",
+    #range=titles[1] + "!A1:YH75",
     majorDimension='ROWS'
     ).execute()
 
+#for summary sheet
 columns = values['values'][4] + ['proc']
+
+def extract_id(filename):
+    start = len('https://docs.google.com/spreadsheets/d/')
+    end = filename.find('/edit?usp=sharing')
+    file_id = filename[start:end]
+    return file_id
+
 df = pd.DataFrame.from_records(values['values'][5:], columns=columns)
+#df = pd.DataFrame.from_records(values['values'])
+students = []
+for index, row in df.iterrows():
+    if row['1 урок']:
+        student = {}
+        student['name'] = row['Имя']
+        student['family'] = row['Фамилия']
+        student['email'] = row['Email']
+        student['lesson_1'] = extract_id(row['1 урок'])
+        student['lesson_2'] = extract_id(row['2 урок'])
+        student['lesson_3'] = extract_id(row['3 урок'])
+        student['lesson_4'] = extract_id(row['4 урок'])
+        student['lesson_5'] = extract_id(row['5 урок'])
+        student['lesson_6'] = extract_id(row['6 урок'])
+        student['lesson_7'] = extract_id(row['7 урок'])
+        student['lesson_8'] = extract_id(row['8 урок'])
+        student['lesson_9'] = extract_id(row['9 урок'])
+        student['lesson_0'] = extract_id(row['0 урок'])
+        students.append(student)
+
+with open('mio4.json', 'w') as f:
+    json.dump(students, f)
+
 df = df.to_html()
--- sheets/views.py
+++ sheets/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .utils import values, df
+from .utils import values, df, students
 
 def index(request):
-	return render(request, 'sheets/index.html', {'values': df})
+	return render(request, 'sheets/index.html', {'values': df, 'students': students})
