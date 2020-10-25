7586189958e8fe7c45cf6979d76b24d38b92b78b
mary_lev <marylevchenko@gmail.com>
2020-10-25 17:06:25 +0300

    add 2 and 3 tasks

--- sheets/lesson_1.py
+++ sheets/lesson_1.py
@@ -36,13 +36,48 @@ titles = [sheet['properties']['title'] for sheet in spreadsheet['sheets']]
 start_feedback = titles[0] + "!A21:E22"
 help_feedback = titles[1] + "!E10:G10"
 razbor = titles[1] + "!B13:B14"
+#task1
+task1_number = titles[2] + "!H2"
 task1_text = titles[2] + "!G3:G8"
+task1_image_title = titles[2] + "!B2"
 task1_image = titles[2] + "!B3"
+task1_answer = titles[2] + "!G10:H10"
+task1_mark = titles[2] + "!K28"
+task1_feedback = titles[2] + "!G31"
+
+task2_number = titles[3] + "!H2"
+task2_text = titles[3] + "!G3:G8"
+task2_image = titles[3] + "!B3"
+task2_answer = titles[3] + "!G10:H10"
+task2_mark = titles[3] + "!K28"
+task2_feedback = titles[3] + "!G31"
+
+task3_answer = titles[4] + "!G10:G15"
+task3_answer_texts = titles[4] + "!H10:H15"
+
+
+#for task1_image
+#values = service.spreadsheets().get(spreadsheetId=spreadsheet_id, ranges=task1_image, includeGridData=True).execute()
+#hyperlink = values['sheets'][0]['data'][0]['rowData'][0]['values'][0]['hyperlink']
+
+#try for task1_answer
+#values = service.spreadsheets().get(spreadsheetId=spreadsheet_id, ranges=task1_answer, includeGridData=True).execute()
 
 values = service.spreadsheets().values().get(
     spreadsheetId=spreadsheet_id,
-    range=task1_image,
+    range=task3_answer_texts,
     #range=titles[1] + "!A1:YH75",
     majorDimension='ROWS',
     ).execute()
+
+if values['values']:
+	values = values['values']
+
+#for task1_answer
+#values = values['values'][0][0]
+
+#for task1_mark:
+#values = values['values'][0][0]
+
+#print(values)
 
