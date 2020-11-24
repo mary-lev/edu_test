102d33e94682746159d503b7ffa0014761a53c9b
mary_lev <marylevchenko@gmail.com>
2020-11-24 10:54:46 +0300

    add new task

--- sheets/tasks.py
+++ sheets/tasks.py
@@ -2,16 +2,16 @@ from celery.schedules import crontab
 from celery import shared_task
 from celery.utils.log import get_task_logger
 
-from sheets import statistics
+from sheets import statistics, check_links
 
 
 @shared_task(
-	run_every=(crontab(minute="*/15")),
+	run_every=(crontab(minute="*/30")),
 	name="save_stats_task",
 	ignore_result=True
 	)
 def save_stats_task():
-	statistics()
+	check_links()
 
 
 
