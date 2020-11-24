0f7f678b3c3741b610e1b617823cffde20ffa739
mary_lev <marylevchenko@gmail.com>
2020-11-24 09:50:44 +0300

    for new celery task

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn edu_test.wsgi
+worker: celery -A edu_test.celery worker -B --loglevel=info
---
+++ sheets/check_links.py
@@ -0,0 +1,78 @@
+import json
+import time
+import string
+import requests
+import httplib2
+import apiclient.discovery
+from oauth2client.service_account import ServiceAccountCredentials
+from sheets.from_navec import analyze_one
+
+
+def create_keyfile_dict():
+    variables_keys = {
+        "type": "service_account",
+        "project_id": "sheetstest-292309",
+        "private_key_id": SHEETS_PRIVATE_KEY_ID,
+        "private_key": SHEETS_PRIVATE_KEY,
+        "client_email": "account@sheetstest-292309.iam.gserviceaccount.com",
+        "client_id": SHEETS_CLIENT_ID,
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url":  "https://www.googleapis.com/robot/v1/metadata/x509/account%40sheetstest-292309.iam.gserviceaccount.com"
+    }
+    return variables_keys
+
+
+
+# Файл, полученный в Google Developer Console
+CREDENTIALS_FILE = 'sheets/sheetstest.p12'
+CREDENTIALS_EMAIL = 'account@sheetstest-292309.iam.gserviceaccount.com'
+KEY = 'notasecret'
+
+scope = ['https://www.googleapis.com/auth/spreadsheets',
+     'https://www.googleapis.com/auth/drive']
+
+try:
+	credentials = ServiceAccountCredentials.from_json_keyfile_dict(
+		create_keyfile_dict(),
+		scope
+		)
+except:
+	credentials = ServiceAccountCredentials.from_p12_keyfile(
+		CREDENTIALS_EMAIL,
+		CREDENTIALS_FILE,
+		KEY,
+		scope,
+		)
+
+
+httpAuth = credentials.authorize(httplib2.Http())
+service = apiclient.discovery.build('sheets', 'v4', http = httpAuth)
+
+
+#статистика текстового модуля за сентябрь
+#spreadsheet_id = '1ZNFzqIpfAVdGdcp2e_e9C5NJz__-NeccbSR-Qjrr__E'
+
+#статистика сценариев за сентябрь
+spreadsheet_id = '11ErqwwqrVdJLNHKgqEuc8rsdnb3jYY1nzCnDAJb38Ug'
+
+# читаем шит
+def main():
+	spreadsheet = service.spreadsheets().get(
+		spreadsheetId=spreadsheet_id,
+		).execute()
+
+	links = service.spreadsheets().values().get(
+		spreadsheetId=spreadsheet_id,
+		range="37!I6:I49",
+		majorDimension='ROWS',
+		).execute()['values']
+
+	return links
+
+
+if __name__ == '__main__':
+	links = main()
+	for link in links:
+		print(link)
