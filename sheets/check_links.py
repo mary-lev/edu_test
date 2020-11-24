ee522d6962e5bc852b19e1fca9a536384fdee4a9
mary_lev <marylevchenko@gmail.com>
2020-11-24 13:04:40 +0300

    another try

--- sheets/check_links.py
+++ sheets/check_links.py
@@ -14,7 +14,7 @@ def create_keyfile_dict():
         "type": "service_account",
         "project_id": "sheetstest-292309",
         "private_key_id": os.environ.get('SHEETS_PRIVATE_KEY_ID'),
-        "private_key": os.environ.get('SHEETS_PRIVATE_KEY').replace('\n', ''),
+        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDXduPfkrWiQOJV\n3A95MNpJoeMo8jmxJJEyJj6xia48J6RjZsOF0h9bmA53scBBRh2NG9t2CNqksfoj\nHOpSXgurMZDODFdB460LtZeyBfwb1q3t0rKhJZzFy5cmmJOx86MG7QbwYw28TFRa\nbwDy5yBfunfufWLgFPJsGcMiduuc6BLtM3dSTm/6zeBRwkelzWZ25/1UuCtlbg86\n2+DCVk9f5yOF3SkWf643mNJrNbGRJgQOSyMB9m3LsHGN9WnpjUXwm+zmGxNsfqfz\n4RdU2j19jm+yTJlDB0WXXtuihTgUQrMpu9FzciqiFY5Dw/dGwpoqf6GrK3Y7BWjA\nGM6BCqtLAgMBAAECggEAEHRu8KsWnf6QingU4m99rDPl0kG1cDbXs9nB3fMi0EtC\nvaiic/zZEjipmjCD+zgsl5MyDnCcqglBjh7uMj4ma/uMEIBWxZxqocNLg3f7ifC6\nNbhqle32bBChSXxLg0KVB7lgV0lpSiEWLBXgH6zRBx6JfDgtLenLnb/V7DLiyYe3\nvJIIPB9VGwjqv71+CWmfKkff89UZif+hqFDiZOcDftEX7I2HvUu7zRLw5Qt3PFj8\nP5DQmEA89YV1N6HOQHg19jn4bCEj2cInXrJvbUYeWB8tQwii5ApVPWV3DC2tj8mH\nkb8oIV/FbuwgM8+i/07WCA9uM0sirDOx77f42+RuwQKBgQD71h1gSc0mfvbMuLdS\nIECKWwOkDRg0N1IDd8wuQLwPEuVkU4U3uCPHLEgBi95yyhEzaVJHRlcpYorm/zPO\nj8LYHMXBvKMNtC/nNciKM/Kv0vWv+N5YJT3bnHKBy/wvbRN2+M/D2chAsJwTVgvZ\nHPSbD8MQNJ0rrsYvSd7KnbtgKwKBgQDbBtUxKZG+zOjwRYEf057v235YlCQQZDdg\nhK7jKOO7Uo1LT7sbAz0bH6KNqd8vPOq9fI+wJpU7N1uEQwDvX8150zntREsQQwXp\nDjR443JkEfEdUOM8Uyy7EKAdzrk6tDkJlzvaA9qQ9b4QfbYiAJZURcfBqNFmdTYP\nzjUTSmkxYQKBgQCNvItaBw9sbCbRP+EFWtuYQvNAoJIPyVbiVslW+t4dCcCLf6pJ\nmtygG58HJ8ZyCqI1skA5lzA1PZ35toZc6nJ6GW9/w4BtlQ0k7xNIHUaoiG34tXPG\nXmd7M4kK3li4BIbg/dZJX0giiIO/Kj2O24obv3pEo6gVzs8CDGxU0wODcwKBgBCY\ntfLhuX+Bu2zpd11YBcUbFrnTj+AGGAQiSbZA1PDO0pIQWpczaZ/yh7jAVH8Z2Je/\nJowsgEZabX7aLOn2knVWwh7ue9mhmoMMZoF6iqJ2kZStGmrpyovqOJfJoFOIcIH/\nibN9c0RkuqA5RmHRNZLxLq/IAQYIf6426+KoUsxBAoGBAIjUgwcyMaEUuY1rRi/W\nMHT9zHSt9FGSuhh661PFwOXZr0ay8B+4UeA4ZHqH5izzS8mVaBEOAtYBE7D/5NXG\nQ0V7S0i6FC5GOTEhSQ7G4C4aqItu/9SyIW5VY/rxDrPdlCyDlA57gk73nUBJDWpz\nT7xRNTajqJObxkbXiNXWwtPm\n-----END PRIVATE KEY-----\n",
         "client_email": "account@sheetstest-292309.iam.gserviceaccount.com",
         "client_id": os.environ.get('SHEETS_CLIENT_ID'),
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
@@ -24,22 +24,26 @@ def create_keyfile_dict():
     }
     return variables_keys
 
-print(create_keyfile_dict())
 
 
 scope = ['https://www.googleapis.com/auth/spreadsheets',
      'https://www.googleapis.com/auth/drive']
 
-CREDENTIALS_FILE = 'sheets/sheetstest.p12'
-CREDENTIALS_EMAIL = 'account@sheetstest-292309.iam.gserviceaccount.com'
-KEY = 'notasecret'
-credentials = ServiceAccountCredentials.from_p12_keyfile(
-	CREDENTIALS_EMAIL,
-	CREDENTIALS_FILE,
-	KEY,
-	scope,
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(
+	create_keyfile_dict(),
+	scope
 	)
-
+"""except:
+	CREDENTIALS_FILE = 'sheets/sheetstest.p12'
+	CREDENTIALS_EMAIL = 'account@sheetstest-292309.iam.gserviceaccount.com'
+	KEY = 'notasecret'
+	credentials = ServiceAccountCredentials.from_p12_keyfile(
+		CREDENTIALS_EMAIL,
+		CREDENTIALS_FILE,
+		KEY,
+		scope,
+		)
+"""
 
 httpAuth = credentials.authorize(httplib2.Http())
 service = apiclient.discovery.build('sheets', 'v4', http = httpAuth)
